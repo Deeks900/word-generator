ff1abc777642c8d71b236f103ab35aef72b57940
Deeks900 <deekshasharma2022@gmail.com>
2022-06-11 12:28:41 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+//We are not using {} in this import of arrayWords because in that we have wrote export default
+import arrayWords from "../utils/words";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+  words = '';
+  limit = 10;
+
+  handleSlideChange(newLimit: number){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0, this.limit).join(" ");
+  }
+
 }
